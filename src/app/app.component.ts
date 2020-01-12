9c5b43aa3d89d05da0d2187ef51a7f2919fa15b7
Guillaume Huertas <guillaumehuertas@free.fr>
2020-01-12 16:23:53 +0100

    3.5 Liens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template:
     `<h1>{{ listName }}</h1>
-    <div>Aucun élément</div>
+    <div [hidden]='todos.length'>Aucun élément</div>
     <ul>
       <li *ngFor='let todo of todos; let i = index; let first = first'>
-        ({{i}}) {{todo}}  ,premier de la liste : {{first}}
+        <hr *ngIf='i>0'>
+        ({{i}}) {{todo}}  : premier de la liste : {{first}}
+            Recherche Google Associé :
+        <a [href]='"http://google.fr/#q=" + todo'
+        target='_blank'>{{todo}}</a>
       </li>
     </ul>
     <form (submit)='createTodo()'>
