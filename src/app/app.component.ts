a5bcfe02e7df443898d9b4a60c65a636aa7f865c
Guillaume Huertas <guillaumehuertas@free.fr>
2020-01-12 15:56:00 +0100

    Todo List Simple OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:
+    `<h1>{{ listName }}</h1>
+    <div>Aucun élément</div>
+    <ul>
+      <li *ngFor='let todo of todos; let i = index; let first = first'>
+        ({{i}}) {{todo}}  ,premier de la liste : {{first}}
+      </li>
+    </ul>
+    <form (submit)='createTodo()'>
+      <input type='text' name='todoLabel' [(ngModel)]='newTodo' />
+      <button (submit)='createTodo()'>ajouter</button>
+    </form>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'firstApp';
+  listName = 'Ma Todo List';
+  todos: string[] =[];
+  newTodo: string  = '';
+
+  createTodo() {
+    if (this.newTodo) {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    }
+  }
 }
